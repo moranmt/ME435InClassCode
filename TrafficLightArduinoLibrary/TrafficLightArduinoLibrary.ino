384ef7a1fefa23cc7558e2277bfa7d670c1af4c9
moranmt <62971225+moranmt@users.noreply.github.com>
2020-04-24 22:01:56 -0400

    initialize arduino sketch

--- TrafficLightArduinoLibrary/TrafficLightArduinoLibrary.ino
+++ TrafficLightArduinoLibrary/TrafficLightArduinoLibrary.ino
@@ -1,8 +1,56 @@
-void setup(){
-//Hello GitHub
+#define	RED_LED		10
+#define	YELLOW_LED	5
+#define	GREEN_LED	6
+
+#define BUTTON 		0
+
+
+#define PRESSED		0
+#define UNPRESSED	1
+
+
+
+
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+  pinMode(YELLOW_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  
+  pinMode(BUTTON, INPUT_PULLUP);
+  
+  digitalWrite(GREEN_LED, HIGH);
+  digitalWrite(RED_LED, HIGH);
+  digitalWrite(YELLOW_LED, HIGH);
+  delay(100);
+  digitalWrite(GREEN_LED, LOW);
+  digitalWrite(RED_LED, LOW);
+  digitalWrite(YELLOW_LED, LOW);
+  delay(100);
+  
+  
 }
 
-void loop(){
+void loop()
+{
+  
+  if(digitalRead(BUTTON) == PRESSED){
+    digitalWrite(GREEN_LED, HIGH);
+    delay(2000);
+    digitalWrite(GREEN_LED, LOW);
+    digitalWrite(YELLOW_LED, HIGH);
+    delay(1000);
+    digitalWrite(YELLOW_LED, LOW);
+    digitalWrite(RED_LED, HIGH);
+    delay(5000);
+    digitalWrite(RED_LED, LOW);
+    
+  }
+  else{
+    digitalWrite(RED_LED, LOW);
+    digitalWrite(GREEN_LED, LOW);
+    digitalWrite(YELLOW_LED, LOW);
+  }
 
 }
-
