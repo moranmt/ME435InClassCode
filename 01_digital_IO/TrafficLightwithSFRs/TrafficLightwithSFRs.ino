515ebe70e21df2317906d4019b2d6973c0dff351
moranmt <62971225+moranmt@users.noreply.github.com>
2020-04-29 20:12:20 -0400

    organized folders

---
+++ 01_digital_IO/TrafficLightwithSFRs/TrafficLightwithSFRs.ino
@@ -0,0 +1,72 @@
+#define	RED_LED		10
+#define	YELLOW_LED	5
+#define	GREEN_LED	6
+
+#define BUTTON 		0
+
+
+#define PRESSED		0
+#define UNPRESSED	1
+
+
+
+
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+  pinMode(YELLOW_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  
+  pinMode(BUTTON, INPUT_PULLUP);
+  
+  
+  //digitalWrite(GREEN_LED, HIGH);
+  PORTD |= _BV(PORTD6); //replaces digitalWrite(GREEN_LED, HIGH);
+  //digitalWrite(RED_LED, HIGH);
+  PORTB |= _BV(PORTB2);  // replaces digitalWrite(RED_LED, HIGH);
+  //digitalWrite(YELLOW_LED, HIGH);
+  PORTB |= _BV(PORTB1);  // replaces digitalWrite(YELLOW_LED, HIGH);
+  delay(100);
+  //digitalWrite(RED_LED, LOW);
+  PORTB &= ~_BV(PORTB2);      //replaces digitalWrite(RED_LED, LOW); or use PORTB = PORTB & B11111011;
+  //digitalWrite(GREEN_LED, LOW);
+  PORTD &= ~_BV(PORTD6); //replaces digitalWrite(GREEN_LED, LOW);
+  //digitalWrite(YELLOW_LED, LOW);
+  PORTB &= ~_BV(PORTB1);// replaces digitalWrite(YELLOW_LED, LOW);
+  delay(100);
+  
+  
+}
+
+void loop()
+{
+  
+  if(digitalRead(BUTTON) == PRESSED){
+    PORTD |= _BV(PORTD6); //replaces digitalWrite(GREEN_LED, HIGH);
+    
+    delay(2000);
+    PORTD &= ~_BV(PORTD6); //replaces digitalWrite(GREEN_LED, LOW);
+    PORTB |= _BV(PORTB1);//replaces digitalWrite(YELLOW_LED, HIGH);
+    delay(1000);
+    PORTB &= ~_BV(PORTB1);// replaces digitalWrite(YELLOW_LED, LOW);
+    
+    
+    PORTB |= _BV(PORTB2);  // replaces digitalWrite(RED_LED, HIGH); or use PORTB = PORTB | B00000100;
+    delay(5000);
+    
+    
+    PORTB &= ~_BV(PORTB2);      //replaces digitalWrite(RED_LED, LOW); or use PORTB = PORTB & B11111011;
+
+    
+  }
+  else{
+    //digitalWrite(RED_LED, LOW);
+    PORTB &= ~_BV(PORTB2);      //replaces digitalWrite(RED_LED, LOW); or use PORTB = PORTB & B11111011;
+    //digitalWrite(GREEN_LED, LOW);
+    PORTD &= ~_BV(PORTD6); //replaces digitalWrite(GREEN_LED, LOW);
+    //digitalWrite(YELLOW_LED, LOW);
+    PORTB &= ~_BV(PORTB1);// replaces digitalWrite(YELLOW_LED, LOW);
+  }
+
+}
